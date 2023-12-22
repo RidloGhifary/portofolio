68f0280d59e403082e04d7e3df0c957a6ab5371d
ridho <ridho@ridhos-MBP.lan>
2023-12-22 17:09:59 +0700

    create footer section

---
+++ app/footer/footer.jsx
@@ -0,0 +1,9 @@
+import variables from './variables.module.scss'
+
+export default function Footer() {
+  return (
+    <footer className={variables.footer}>
+      <p>Copyright © 2023 All Rights Reserved.</p>
+    </footer>
+  )
+}
