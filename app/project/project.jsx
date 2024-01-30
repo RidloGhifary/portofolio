66af0154ae8d679c077cdf60f1aeea508e961c36
ridho <ridloghfry@gmail.com>
2024-01-30 17:49:41 +0700

    update link button

--- app/project/project.jsx
+++ app/project/project.jsx
@@ -1,9 +1,9 @@
-import variables from '../variables.module.scss'
-import gymSyncBg from '/public/gymsync.png'
-import carRentalBg from '/public/carrental.png'
-import Image from 'next/image'
-import Link from 'next/link'
-import { ArrowSquareOut, GithubLogo } from '@phosphor-icons/react/dist/ssr'
+import variables from "../variables.module.scss";
+import gymSyncBg from "/public/gymsync.png";
+import carRentalBg from "/public/carrental.png";
+import Image from "next/image";
+import Link from "next/link";
+import { ArrowSquareOut, GithubLogo } from "@phosphor-icons/react/dist/ssr";
 
 export default function Project() {
   return (
@@ -16,18 +16,36 @@ export default function Project() {
       <div className={variables.listProjects}>
         <div className={variables.project}>
           <div className={variables.imageBox}>
-            <Image src={gymSyncBg} alt="gymsync" width={500}
-              height={500} placeholder="blur" className={variables.image} />
+            <Image
+              src={gymSyncBg}
+              alt="gymsync"
+              width={500}
+              height={500}
+              placeholder="blur"
+              className={variables.image}
+            />
           </div>
           <div className={variables.projectDesc}>
             <h1>GymSync</h1>
-            <p>GymSync is a fitness-oriented term that could be associated with activities related to synchronizing or coordinating workouts, exercise routines, or fitness tracking. It might involve the integration of technology, apps, or devices to streamline and optimize fitness experiences. </p>
+            <p>
+              GymSync is a fitness-oriented term that could be associated with
+              activities related to synchronizing or coordinating workouts,
+              exercise routines, or fitness tracking. It might involve the
+              integration of technology, apps, or devices to streamline and
+              optimize fitness experiences.{" "}
+            </p>
             <div className={variables.linkProject}>
-              <Link href='https://gymsync.vercel.app/' target='_blank' className={variables.projectLink}>
+              <Link
+                href="https://gymsync.vercel.app/"
+                target="_blank"
+                className={variables.projectLink}>
                 <ArrowSquareOut size={28} color="#cfd0d5" weight="bold" />
                 <span>Demo</span>
               </Link>
-              <Link href='https://gymsync.vercel.app/' target='_blank' className={variables.projectLink}>
+              <Link
+                href="https://github.com/RidloGhifary/gymsync"
+                target="_blank"
+                className={variables.projectLink}>
                 <GithubLogo size={28} color="#cfd0d5" weight="fill" />
                 <span>Code</span>
               </Link>
@@ -36,18 +54,36 @@ export default function Project() {
         </div>
         <div className={variables.project}>
           <div className={variables.imageBox}>
-            <Image src={carRentalBg} alt="carRentalBg" width={500}
-              height={500} placeholder="blur" className={variables.image} />
+            <Image
+              src={carRentalBg}
+              alt="carRentalBg"
+              width={500}
+              height={500}
+              placeholder="blur"
+              className={variables.image}
+            />
           </div>
           <div className={variables.projectDesc}>
             <h1>Car Rental</h1>
-            <p>GymSync is a fitness-oriented term that could be associated with activities related to synchronizing or coordinating workouts, exercise routines, or fitness tracking. It might involve the integration of technology, apps, or devices to streamline and optimize fitness experiences. </p>
+            <p>
+              GymSync is a fitness-oriented term that could be associated with
+              activities related to synchronizing or coordinating workouts,
+              exercise routines, or fitness tracking. It might involve the
+              integration of technology, apps, or devices to streamline and
+              optimize fitness experiences.{" "}
+            </p>
             <div className={variables.linkProject}>
-              <Link href='https://car-rental-fawn-iota.vercel.app/' target='_blank' className={variables.projectLink}>
+              <Link
+                href="https://car-rental-fawn-iota.vercel.app/"
+                target="_blank"
+                className={variables.projectLink}>
                 <ArrowSquareOut size={28} color="#cfd0d5" weight="bold" />
                 <span>Demo</span>
               </Link>
-              <Link href='https://github.com/RidloGhifary/car-rental' target='_blank' className={variables.projectLink}>
+              <Link
+                href="https://github.com/RidloGhifary/car-rental"
+                target="_blank"
+                className={variables.projectLink}>
                 <GithubLogo size={28} color="#cfd0d5" weight="fill" />
                 <span>Code</span>
               </Link>
@@ -56,5 +92,5 @@ export default function Project() {
         </div>
       </div>
     </section>
-  )
+  );
 }
