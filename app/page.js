fa4b9411871c0663625f66eb408147cf814c3eee
ridho <ridloghfry@gmail.com>
2024-01-30 17:42:48 +0700

    update ui

--- app/page.js
+++ app/page.js
@@ -1,14 +1,19 @@
-import Footer from './footer/footer'
-import Header from './header/header'
-import Project from './project/project'
-import variables from './variables.module.scss'
+import Footer from "./footer/footer";
+import Header from "./header/header";
+import Project from "./project/project";
+import variables from "./variables.module.scss";
 
 export default function Home() {
   return (
     <div className={variables.container}>
+      <div className={variables.lines}>
+        <div className={variables.line}></div>
+        <div className={variables.line}></div>
+        <div className={variables.line}></div>
+      </div>
       <Header />
       <Project />
       <Footer />
     </div>
-  )
+  );
 }
